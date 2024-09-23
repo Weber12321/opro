21a55293074616c5fc664e7e0317ca5a97b5e4ab
Your Name <you@example.com>
2024-09-23 03:53:49 +0000

    update the requirements with denpendency

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
+import logging
 import re
 import pandas as pd
+import torch
 
 from Prompts import meta_prompt, scorer_prompt
 from langchain import PromptTemplate
@@ -10,22 +12,35 @@ from transformers import pipeline
 
 
 def create_chain_from_template(
-    template, input_variables, temperature=.5,callbacks=[], verbose=True):
+    template, 
+    input_variables, 
+    temperature=0.5,
+):
+    logging.info(f"Creating chain from template: {template}")
 
     prompt = PromptTemplate(
         input_variables=input_variables,
         template=template
     )
 
-    hf = HuggingFacePipeline(pipeline=pipeline(
-        model="/opt/llm/models/Breeze-7B-32k-Instruct-v1_0" ,
-        task="text-generation"
-    )) 
+    logging.info("Read model")
+    hf = HuggingFacePipeline(pipeline=
+        pipeline(
+            task="text-generation",
+            model="/opt/llm/models/Breeze-7B-32k-Instruct-v1_0" ,
+            model_kwargs={
+                "torch_dtype": torch.bfloat16,
+            },
+            device_map="auto",
+            temperature=temperature,
+            do_sample=True,
+            return_full_text=False,
+        )
+    ) 
 
     chain = prompt | hf | StrOutputParser()
 
     return chain
-
 
 def build_text_and_scores(performance_df):
     return ''.join([f"text:\n{performance_df.iloc[i]['text']}\nscore:\n{performance_df.iloc[i]['score']}\n" for i in range(len(performance_df))])
@@ -41,7 +56,9 @@ def sample_exemplars(training_exemplar_df,num_exemplars=3):
     return ''.join([f"input:\nQ: {exemplar_sample.iloc[i]['question']}\nA: <INS>\noutput:\n{exemplar_sample.iloc[i]['raw_answer']}\n" for i in range(num_exemplars)])
 
 def generate_prompts(optimizer_chain,texts_and_scores,exemplars,n_prompts=3):
-    return [optimizer_chain.predict(texts_and_scores=texts_and_scores,exemplars=exemplars).replace("[","").replace("]","") for i in range(n_prompts)]
+    return [optimizer_chain.invoke(
+        {"texts_and_scores": texts_and_scores, "exemplars": exemplars},
+        ).replace("[","").replace("]","") for _ in range(n_prompts)]
 
 def are_numbers_the_same(x,y):
     return ''.join(re.findall(r'\d+',x)) == ''.join(re.findall(r'\d+',y))
@@ -49,7 +66,7 @@ def are_numbers_the_same(x,y):
 def score_prompts(scorer_chain,prompts,training_examples,performance_df):
     for prompt in prompts:
         scores = []
-        for index, example in training_examples.iterrows():
+        for _, example in training_examples.iterrows():
             question = example['question']
             answer = example['raw_answer']
             sample_answer = scorer_chain.predict(question=question,instruction=prompt)
@@ -71,14 +88,16 @@ def opro(optimizer_chain,scorer_chain,performance_df,training_exemplar_df,n_scor
 
 
 if __name__ == '__main__':
-    optimizer_chain = create_chain_from_template(meta_prompt,
-                                                 ["texts_and_scores","exemplars"],
-                                                 temperature=1,
-                                                 verbose=True)
-    scorer_chain = create_chain_from_template(scorer_prompt,
-                                                ["question","instruction"],
-                                                temperature=0,
-                                                verbose=True)
+    optimizer_chain = create_chain_from_template(
+        meta_prompt,
+        ["texts_and_scores","exemplars"],
+        temperature=0.5,
+    )
+    scorer_chain = create_chain_from_template(
+        scorer_prompt,
+        ["question","instruction"],
+        temperature=0.0,
+    )
     performance_df = pd.read_csv("data/performance.csv",index_col=0)
 
     training_exemplar_df = pd.read_csv("data/training_exemplars.csv",index_col=0)
